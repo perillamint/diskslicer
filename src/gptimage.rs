7177c0c8a4aa8312b11dba28339f906de7654c1e
perillamint <perillamint@gentoo.moe>
2016-05-21 22:55:20 +0900

    Add GPTHeader

--- src/gptimage.rs
+++ src/gptimage.rs
@@ -4,7 +4,27 @@ pub struct GPTImage {
     disk_image: Mmap,
 }
 
-static GPT_MAGIC: [u8; 8] = [0x45, 0x46, 0x49, 0x20, 0x50, 0x41, 0x52, 0x54];
+//Names are copied from Linux block/partitions/efi.h
+//https://github.com/torvalds/linux/blob/9256d5a308c95a50c6e85d682492ae1f86a70f9b/block/partitions/efi.h#L70
+#[repr(C)]
+struct GPTHeader {
+    signature: [u8; 8],
+    revision: [u8; 4],
+    header_size: i32,
+    header_crc32: [u8; 4],
+    reserved1: [u8; 4],
+    my_lba: u64,
+    alternate_lba: u64,
+    first_usable_lba: [u8; 8],
+    last_usable_lba: [u8; 8],
+    guid: [u8; 16],
+    partition_entry_lba: u64,
+    num_partition_entries: u32,
+    sizeof_partition_entry: u32,
+    partition_entry_array_crc32: [u8; 4],
+}
+
+static GPT_SIGNATURE: [u8; 8] = [0x45, 0x46, 0x49, 0x20, 0x50, 0x41, 0x52, 0x54];
 
 impl GPTImage {
     fn new(disk_image_filepath: String) -> GPTImage {
@@ -13,8 +33,27 @@ impl GPTImage {
 
         //TODO: Do some sanity check here.
 
+        //Get [u8] slice.
+        let image_slice = unsafe{ disk_image.as_slice(); };
+        
+        //Check magic.
+//        image_slice 
+        
+
         GPTImage {
             disk_image: disk_image,
         }
     }
+}
+
+#[test]
+#[should_panic]
+fn file_not_found_test() {
+    let gpt_image = GPTImage::new("test_disk_image_not_exist.img".to_string());
+}
+
+#[test]
+fn gpt_image_sanity_check() {
+    //TODO: Do sanity check.
+    let gpt_image = GPTImage::new("test_disk_image_gpt.img".to_string());
 }
